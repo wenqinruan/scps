a578807f09c89e681c9e1a786a2d477f74891dbe
ruanwenqin <ruanwenqin@howzhi.com>
2016-10-11 14:54:53 +0800

    修复search只search单个列的bug

--- client.php
+++ client.php
@@ -4,8 +4,8 @@ if (!$client->connect('127.0.0.1', 9501, -1)) {
     exit("connect failed. Error: {$client->errCode}\n");
 }
 $data = array(
-    'method' => 'fetchAll',
-    'args' => array('SELECT * FROM course WHERE parentId =1 AND locked = 1'),
+    'method' => 'executeQuery',
+    'args' => array('SELECT count(id) FROM groups_thread groups_thread'),
 );
 $client->send(json_encode($data).'\r\n\r\n');
 $data = $client->recv() . "\n";
--- src/DBALCaller.php
+++ src/DBALCaller.php
@@ -10,13 +10,15 @@ class DBALCaller
         $result = array();
         if ($method == 'executeUpdate') {
             $result['affectedRows'] = $statement;
-
+            //insert 要返回lastInsertId
             if ('INSERT' == strtoupper(substr(ltrim($args[0]), 0, 6))) {
                 $result['lastInsertId'] = $conn->lastInsertId();
             }
  
         } elseif ($method == 'executeQuery') {
-            if ($statement->columnCount() == 1) {
+            //executeQuery 主要是 querybuilder
+            $meta = $statement->getColumnMeta(0);
+            if ('COUNT' == strtoupper(substr(ltrim($meta['name']), 0, 5))) {
                 $result['fetchColumn'] = $statement->fetchColumn(0);
             } else {
                 $result['fetchAll'] = $statement->fetchAll();
